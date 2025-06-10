12530056018d80c606af45fdeeabc14e475f68e0
Bogdan Woźniak <wozbog@gmail.com>
2025-06-10 21:38:48 +0200

    brute force

--- operations.py
+++ operations.py
@@ -14,5 +14,28 @@ def print_help():
 def dp_knapsack():
     print("Dynamic programming")
 
-def brute_force():
-    print("Brute Force")
+def brute_force(C, items):
+    n = len(items)
+    best_value = 0
+    best_comb = []
+
+    for i in range(1 << n):
+        current_c = 0
+        current_value = 0
+        current_comb = []
+
+        for j in range(n):
+            if i & (1 << j):
+                p, w = items[j]
+                current_c += w
+                current_value += p
+                current_comb.append(j)
+
+                if current_c > C:
+                    break
+
+        if current_c <= C and current_value > best_value:
+            best_value = current_c
+            best_comb = current_c
+
+    return best_value, best_comb
