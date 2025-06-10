68d9a61eecb03338c53f07bf1e96ebb2396b1a2a
Bogdan Woźniak <wozbog@gmail.com>
2025-06-10 18:12:12 +0200

    User input

--- main.py
+++ main.py
@@ -11,23 +11,52 @@ def main():
     #Variables
     capacity = 0
     n = 0
+    items = []
 
     #User input
     try:
+        
+        #Capacity of knapsack
         print("Input capacity of a knapsack")
         capacity = int(input("C> ").strip())
         if capacity <= 0:
             print(f"{capacity} isn't greater than 0")
             sys.exit(1)
+        
+        #Number of items
         print("Input a number of items")
         n = int(input("n> ").strip())
-        if (n < 0):
+        if (n <= 0):
             print("Number of items must be greater than 0 and be an integer")
             sys.exit(1)
+
+        #Values of items    
+        print(f"Input {n} items in the format: p(value) w(capacity) ")
+        for i in range(n):
+            while True:
+                try:
+                    print(f"\nItem {i+1}/{n}")
+                    data = input("p w> ").split()
+                    if len(data) < 2:
+                        print("Enter both value and capacity")
+                        continue
+
+                    p = int(data[0].strip())
+                    w = int(data[1].strip())
+
+                    if p<=0 or w<=0:
+                        print("Values of p and w must be greater than 0")
+                        continue
+
+                    items.append((p, w))
+                    break
+
+                except ValueError:
+                    print("Invalid input. Please enter integers.")
+    
     except Exception as e:
         print(f"Invalid input: {e}")
         sys.exit(1) 
-
 
     #Reset terminal
     try:
