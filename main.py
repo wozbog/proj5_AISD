e74425a796f3aa861419bf14c01db9dbbabd8691
Bogdan Woźniak <wozbog@gmail.com>
2025-06-10 16:54:09 +0200

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import threading
 sys.setrecursionlimit(1000000000)
 
 def main():
-    print("initial commit")
+    # Command-line argument: python3 mainpy --knapsack
+    if len(sys.argv) != 2 or (sys.argv[1] != "--knapsack"):
+        print("Usage: python3 main.py --knapsack")
+        sys.exit(1)
+
+    #Variables
+    
 
 
 if __name__ == "__main__":
